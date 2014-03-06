f68a7d02bd8580276be6b9043b2c1ec30e976733
Orry Baram <orrybaram@gmail.com>
2014-03-06 17:49:35 -0500

    working better

--- chrome/content.js
+++ chrome/content.js
@@ -10,41 +10,45 @@ $(function() {
         if(isExternal(url)) {
             console.log($(this))
 
-            if ($this[0].host === 'en.wikipedia.org') {
-                // fetchRemoteData($this, url, function(data) {
-                //     console.log(data)
-                // })
-            }
-            else if ( ( url.indexOf(".jpg") > 0 ) || ( url.indexOf(".png") > 0 ) || ( url.indexOf(".gif") > 0 ) ) {
-                $this.css({'border-bottom': '1px dotted'});
-                $this.hover(function() {
+            if ( ( url.indexOf(".jpg") > 0 ) || ( url.indexOf(".png") > 0 ) || ( url.indexOf(".gif") > 0 ) ) {
+                $this.css({'backgroundColor': 'yellow'});
+
+                hoverDelay($this, function() {
                     var image = new Image();
                     image.src = url;
-                    var imgHeight = image.height;
-                    var imgWidth = image.width;
                     $previewer.html(image);
-
+                    $previewer.css({ 'top': $this.offset().top, 'left': $this.offset().left + $this.width() + 25 })
+                    $previewer.fadeIn();
+                }, function() {
+                    $previewer.fadeOut();
                 })
-
             } else {
-                 console.log('nope')
+                hoverDelay($this, function() {
+                    $.get(url).success(function(data) {
+                        console.log(data)
+                    })
+                });
             }
-
         }
     });
 });
-
-function fetchRemoteData(context, url, callback) {
-    context.hover(function() {
-        $.get(url).success(function(data) { callback(data) })
-    })
-
-}
-
 
 function isExternal(url) {
     var match = url.match(/^([^:\/?#]+:)?(?:\/\/([^\/?#]*))?([^?#]+)?(\?[^#]*)?(#.*)?/);
     if (typeof match[1] === "string" && match[1].length > 0 && match[1].toLowerCase() !== location.protocol) return true;
     if (typeof match[2] === "string" && match[2].length > 0 && match[2].replace(new RegExp(":("+{"http:":80,"https:":443}[location.protocol]+")?$"), "") !== location.host) return true;
     return false;
+}
+
+function hoverDelay($el, fn, fn2, delay) {
+    var delay_time = 500;
+    if (delay) delay_time = delay;
+    $el.hover(function() {
+        window.hoverTimeout = setTimeout(function() {
+            fn();
+        }, delay_time)
+    }, function() {
+        clearTimeout(window.hoverTimeout)
+        fn2();
+    })
 }
