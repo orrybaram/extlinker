954c95b5a36f5a5f2efca66a8a81c1b9bd4f3177
Orry Baram <orrybaram@gmail.com>
2014-03-06 23:33:15 -0500

    pics load better now

--- chrome/content.js
+++ chrome/content.js
@@ -1,57 +1,73 @@
 // Content
 $(function() {
 
+    // Check to see that we're not on an secured site, otherwise this thing wont work
+    if (window.location.protocol !== "https:") {
+        var $previewer_container = $('<div id="link-previewer-container"></div>');
+        var $previewer = $('<div id="link-previewer"></div>');
 
-    var $previewer_container = $('<div id="link-previewer-container"></div>');
-    var $previewer = $('<div id="link-previewer"></div>');
+        $previewer_container.append($previewer);
+        $('body').append($previewer_container);
 
-    $previewer_container.append($previewer);
-    $('body').append($previewer_container);
+        $('p').find('a').each(function() {
+            var $this = $(this);
+            var url = $(this).attr('href');
+            if(isExternal(url) && url.indexOf('.zip') < 0) {
+                console.log($(this))
 
-    $('p').find('a').each(function() {
-        var $this = $(this);
-        var url = $(this).attr('href');
-        if(isExternal(url) && url.indexOf('.zip') < 0) {
-            console.log($(this))
+                $this.css({
+                  'backgroundColor': shadeRGBColor($this.css('color'), .97),
+                  'border-bottom': '1px solid '+ shadeRGBColor($this.css('color'), .7),
+                  'text-decoration': 'none',
+                  'display': 'inline-block'
+                })
 
-            $this.css({'backgroundColor': 'yellow'});
-            hoverDelay($this, function() {
+                hoverDelay($this, function() {
 
-                // URL IS AN IMAGE
-                if ( ( url.indexOf(".jpg") > 0 ) || ( url.indexOf(".png") > 0 ) || ( url.indexOf(".gif") > 0 ) ) {
-                    var image = new Image();
-                    image.src = url;
-                    $previewer.html(image);
-                    $previewer.css({ 'height': 'auto' })
-                }
+                    // URL IS AN IMAGE
+                    if ( ( url.indexOf(".jpg") > 0 ) || ( url.indexOf(".png") > 0 ) || ( url.indexOf(".gif") > 0 ) ) {
+                        var image = new Image();
+                        image.src = url;
+                        $previewer.html(image);
+                        $(image).load(function() {
+                            $previewer.css({ 'height': image.height, 'width': image.width })
+                        })
+                    }
 
-                else if ($this[0].host === 'www.youtube.com') {
-                    var video_id = getParmFromHash(url, 'v')
-                    console.log(video_id)
-                    $previewer.html('<iframe src="//www.youtube.com/embed/'+ video_id +'"></iframe>');
-                    $previewer.css({ 'width': '640', 'height': '360' })
-                }
+                    else if ($this[0].host === 'www.youtube.com') {
+                        var video_id = getParmFromHash(url, 'v')
+                        console.log(video_id)
+                        $previewer.html('<iframe src="//www.youtube.com/embed/'+ video_id +'"></iframe>');
+                        $previewer.css({ 'width': '1067', 'height': '600' })
+                    }
 
-                // URL IS NORMAL
-                else {
-                    $previewer.css({ 'width': '800', 'height': '500' })
-                    $previewer.html('<iframe src="'+ url +'"></iframe>');
-                }
-                setTimeout(function() {
-                    $previewer.css({ 'margin-top': -$previewer.height() / 2, 'margin-left': -$previewer.width() / 2 })
-                },100)
+                    // URL IS AN EXTERNAL LINK
+                    else {
 
-                $previewer_container.fadeIn();
+                        $previewer.css({ 'width': window.innerWidth - 300, 'height': window.innerHeight - 100 })
+                        $previewer.html('<iframe src="'+ url +'"></iframe>');
+                    }
+                    setTimeout(function() {
+                        if ($previewer.height() < window.innerHeight) {
+                            $previewer.css({ 'top': '50%', 'margin-top': -$previewer.height() / 2, 'margin-left': -$previewer.width() / 2 })
+                        } else {
+                            $previewer.css({'top':'20px', 'margin-top': '0'})
+                        }
+                    },100)
 
-            }, function() {
-                setTimeout(function() {
-                    checkIfHovered($previewer, function() {
-                        $previewer_container.fadeOut();
-                    })
-                }, 100)
-            });
-        }
-    });
+                    $previewer_container.fadeIn();
+
+                }, function() {
+                    setTimeout(function() {
+                        checkIfHovered($previewer, function() {
+                            $previewer_container.fadeOut();
+                            $previewer.find('iframe').remove();
+                        })
+                    }, 100)
+                });
+            }
+        });
+  }
 });
 
 function isExternal(url) {
@@ -87,4 +103,9 @@ function getParmFromHash(url, parm) {
     var re = new RegExp("[?&]" + parm + "=([^&]+)(&|$)");
     var match = url.match(re);
     return(match ? match[1] : "");
+}
+
+function shadeRGBColor(color, percent) {
+    var f=color.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
+    return "rgb("+(Math.round((t-R)*p)+R)+","+(Math.round((t-G)*p)+G)+","+(Math.round((t-B)*p)+B)+")";
 }
